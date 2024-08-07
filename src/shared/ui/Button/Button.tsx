2849ffd517222797baf6a218b23a8b5f2a53f79c
Bagdat <clonn262@gmail.com>
2024-08-07 16:50:43 +0500

    new

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -1,7 +1,25 @@
-export default function Button() {
-  return (
-    <button>
-      
-    </button>
-  )
+import React from "react";
+import styles from "./styles.module.scss";
+
+export enum ButtonSize {
+  M = "size_m",
+  L = "size_l",
+  XL = "size_xl",
+  Modal = "size_modal",
+  Project = "size_project",
 }
+
+interface IButton extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode | string;
+  size?: ButtonSize;
+}
+
+const Button = ({ children, ...rest }: IButton) => {
+  return (
+    <button type="submit" {...rest}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
--- src/shared/ui/Input/Input.tsx
+++ src/shared/ui/Input/Input.tsx
@@ -1,7 +1,30 @@
-export default function Input() {
-  return (
-    <div>
-      <input type="text" />
-    </div>
-  )
+import React, { InputHTMLAttributes, ReactNode } from "react";
+import cn from "classnames";
+import styles from "./styles.module.scss";
+
+interface IInputProps extends InputHTMLAttributes<HTMLInputElement> {
+  bgColor?: string;
+  className?: string;
+  classField?: string;
+  children?: ReactNode;
 }
+
+const Input: React.FC<IInputProps> = ({
+  className,
+  classField,
+  children,
+  ...rest
+}) => {
+  return (
+    <div className={cn(classField, styles.field)}>
+      <input
+        autoComplete="off"
+        className={cn(className, "outline-none w-full", styles.field__input)}
+        {...rest}
+      />
+      {children}
+    </div>
+  );
+};
+
+export default Input;
