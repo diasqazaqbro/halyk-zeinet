10aa11a7f80b5ce9a9eb37c03e7ac91939d77534
Bagdat <clonn262@gmail.com>
2024-08-08 01:27:37 +0500

    fix

--- src/widgets/PensionAnnuity/ui/PensionAnnuity.tsx
+++ src/widgets/PensionAnnuity/ui/PensionAnnuity.tsx
@@ -8,17 +8,15 @@ import Image from "next/image";
 const PensionAnnuity = () => {
   return (
     <section className={cn(styles.pension)}>
-      <div className="container flex justify-between items-center">
-        <div className="flex flex-col gap-20">
+      <div className="container flex justify-between items-center gap-7">
+        <div className="flex flex-col gap-20 w-[45%]">
           <Heading component="h1">Пенсионный Аннуитет</Heading>
-          <Heading className="w-[70%]" component="h2">
-            Ваш надёжный финансовый партнёр
-          </Heading>
-          <Button size={ButtonSize.L} className=" ">
+          <Heading component="h2">Ваш надёжный финансовый партнёр</Heading>
+          <Button className="w-[300px]" size={ButtonSize.L}>
             Оставить заявку
           </Button>
         </div>
-        <div className="mb-[-1.5%]">
+        <div className="">
           <Image
             className={styles.img}
             width={742}
