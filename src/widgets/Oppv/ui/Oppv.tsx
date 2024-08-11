450baed4872e5bef7b87a903dcd322b6b9d4e314
Bagdat <clonn262@gmail.com>
2024-08-12 02:48:25 +0500

    mob

--- src/widgets/Oppv/ui/Oppv.tsx
+++ src/widgets/Oppv/ui/Oppv.tsx
@@ -5,16 +5,17 @@ import { OppvData } from "@/shared/data/OppvData";
 import Image from "next/image";
 import Button from "@/shared/ui/Button/Button";
 import GreenArrowRight from "@/shared/ui/Icon/Arrow/GreenArrowRight";
+import OrangeArrowRight from "@/shared/ui/Icon/Arrow/OrangeArrowRight";
 
 const Oppv = () => {
   return (
     <section className={styles.oppv}>
-      <div className="container flex justify-between relative">
-        <div className={styles.coin_1} />
-        <div className={styles.coin_2} />
+      <div className="container flex justify-between relative mmd:flex-col">
+        {/* <div className={styles.coin_1} /> */}
+        {/* <div className={styles.coin_2} /> */}
         <div className="flex flex-col justify-between">
           <div className="flex flex-col gap-5 ">
-            <Heading component="h1" className="">
+            <Heading component="h1" className="mmd:w-[80%]">
               А кто такие ОПВ и ОППВ?
             </Heading>
             <ul className="list-disc flex flex-col gap-5 pl-6">
@@ -25,7 +26,7 @@ const Oppv = () => {
               ))}
             </ul>
           </div>
-          <div className={styles.oppv__underleft}>
+          {/* <div className={styles.oppv__underleft}>
             <div className="absolute top-[-114%] left-[29%]">
               <button aria-label="Нажми сюда" className={styles.btn__help}>
                 Нажми сюда
@@ -35,15 +36,18 @@ const Oppv = () => {
               <GreenArrowRight />
             </button>
             <p className={styles.oppv__text}>И сейчас он вам расскажет</p>
-          </div>
+          </div> */}
         </div>
         <div className={styles.oppv__right}>
-          <div className={styles.coin_3} />
-          <div className={styles.who__is}>
+          {/* <div className={styles.coin_3} /> */}
+          {/* <div className={styles.who__is}>
             <button aria-label="Кто" className={styles.who__btn}>
               Диас
             </button>
-          </div>
+          </div> */}
+          <button className={styles.btn__mob} aria-label="Подробнее">
+            <OrangeArrowRight />
+          </button>
           <Image
             className={styles.img}
             width={840}
