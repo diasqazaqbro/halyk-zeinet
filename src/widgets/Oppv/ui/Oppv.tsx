b17899e53a87569c04208a1a4032889014276dc3
Bagdat <clonn262@gmail.com>
2024-08-14 01:50:03 +0500

    fix

--- src/widgets/Oppv/ui/Oppv.tsx
+++ src/widgets/Oppv/ui/Oppv.tsx
@@ -11,10 +11,10 @@ const Oppv = () => {
   return (
     <section className={styles.oppv}>
       <div className="container flex justify-between relative mmd:flex-col">
-        {/* <div className={styles.coin_1} /> */}
-        {/* <div className={styles.coin_2} /> */}
+        <div className={styles.coin_1} />
+        <div className={styles.coin_2} />
         <div className="flex flex-col justify-between">
-          <div className="flex flex-col gap-5 ">
+          <div className="flex flex-col gap-5">
             <Heading component="h1" className="mmd:w-[80%]">
               А кто такие ОПВ и ОППВ?
             </Heading>
@@ -25,8 +25,9 @@ const Oppv = () => {
                 </li>
               ))}
             </ul>
+            <p className={styles.oppv__mobtext}>И сейчас он вам расскажет</p>
           </div>
-          {/* <div className={styles.oppv__underleft}>
+          <div className={styles.oppv__underleft}>
             <div className="absolute top-[-114%] left-[29%]">
               <button aria-label="Нажми сюда" className={styles.btn__help}>
                 Нажми сюда
@@ -36,15 +37,20 @@ const Oppv = () => {
               <GreenArrowRight />
             </button>
             <p className={styles.oppv__text}>И сейчас он вам расскажет</p>
-          </div> */}
+          </div>
         </div>
         <div className={styles.oppv__right}>
-          {/* <div className={styles.coin_3} /> */}
-          {/* <div className={styles.who__is}>
+          <div className={styles.coin_3} />
+          <div className={styles.mobcoin_1} />
+          <div className={styles.mobcoin_2} />
+          <button className={styles.btn__dias}>
+            Диас
+          </button>
+          <div className={styles.who__is}>
             <button aria-label="Кто" className={styles.who__btn}>
               Диас
             </button>
-          </div> */}
+          </div>
           <button className={styles.btn__mob} aria-label="Подробнее">
             <OrangeArrowRight />
           </button>
