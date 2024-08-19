c604a25d6df13ede59c1492a482e8c97860057cc
Bagdat <clonn262@gmail.com>
2024-08-19 21:38:44 +0500

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import MainPage from "@/pages/MainPage";
+import { Footer } from "@/widgets/Footer";
 
 export default function Home() {
   return (
     <main className="wrapper">
       <MainPage />
+      <Footer />
     </main>
   );
 }
--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,6 +1,7 @@
 import { Annuity } from "@/widgets/Annuity";
 import { Benefits } from "@/widgets/Benefits";
 import { Faq } from "@/widgets/FAQ";
+import { Footer } from "@/widgets/Footer";
 import { Oppv } from "@/widgets/Oppv";
 import { OpvOppv } from "@/widgets/OpvOppv";
 import { PensionAnnuity } from "@/widgets/PensionAnnuity";
