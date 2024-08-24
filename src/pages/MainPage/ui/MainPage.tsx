0ef0338695844b5dc2a1f322c20afbed6a2f4963
diasqazaqbro <diasqazaqbro@mail.ru>
2024-08-24 20:06:42 +0500

    remove library

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,15 +1,12 @@
-"use client";
 import { Annuity } from "@/widgets/Annuity";
 import { Benefits } from "@/widgets/Benefits";
 import { Faq } from "@/widgets/FAQ";
-import smoothscroll from "smoothscroll-polyfill";
 import { Oppv } from "@/widgets/Oppv";
 import { OpvOppv } from "@/widgets/OpvOppv";
 import { PensionAnnuity } from "@/widgets/PensionAnnuity";
 import { Security } from "@/widgets/Security";
 //
 export default function MainPage() {
-  smoothscroll.polyfill();
   return (
     <>
       <PensionAnnuity />
