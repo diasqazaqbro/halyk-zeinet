649335abdcf7058c1178f5ac4003337557e821ed
Bagdat <clonn262@gmail.com>
2024-08-26 12:42:32 +0500

    fix

--- src/shared/ui/CodeInput/CodeInput.tsx
+++ src/shared/ui/CodeInput/CodeInput.tsx
@@ -1,5 +1,12 @@
-import React, { ChangeEvent, ClipboardEvent, FC, KeyboardEvent, useEffect, useRef } from 'react';
-import styles from './styles.module.scss';
+import React, {
+  ChangeEvent,
+  ClipboardEvent,
+  FC,
+  KeyboardEvent,
+  useEffect,
+  useRef,
+} from "react";
+import styles from "./styles.module.scss";
 
 type CodeinputProps = { count: number; error?: string; onChange: any };
 
@@ -12,7 +19,11 @@ const CodeInput: FC<CodeinputProps> = ({ count, error, onChange }) => {
 
   const setFocus = (id: string) => {
     const numberId = Number(id);
-    if (inputsRef.current[numberId].value && Number(id) + 1 < count && !inputsRef.current[numberId + 1].value) {
+    if (
+      inputsRef.current[numberId].value &&
+      Number(id) + 1 < count &&
+      !inputsRef.current[numberId + 1].value
+    ) {
       inputsRef.current[numberId + 1].focus();
     }
   };
@@ -20,28 +31,28 @@ const CodeInput: FC<CodeinputProps> = ({ count, error, onChange }) => {
   const handleOnKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
     const { key } = e;
     const target = e.target as HTMLInputElement;
-    if (key === 'Backspace') {
-      if (target.value === '') {
+    if (key === "Backspace") {
+      if (target.value === "") {
         if (target.previousElementSibling !== null) {
           const t = target.previousElementSibling as HTMLInputElement;
-          t.value = '';
+          t.value = "";
           t.focus();
           e.preventDefault();
         }
       } else {
-        target.value = '';
+        target.value = "";
       }
       sendResult();
     }
   };
 
   const sendResult = () => {
-    const res = inputsRef.current.map((input) => input.value).join('');
+    const res = inputsRef.current.map((input) => input.value).join("");
     onChange(res);
   };
 
   const handleOnPaste = (e: ClipboardEvent<HTMLInputElement>) => {
-    const pastedValue = e.clipboardData.getData('Text');
+    const pastedValue = e.clipboardData.getData("Text");
 
     let currentInput = 0;
 
@@ -51,7 +62,10 @@ const CodeInput: FC<CodeinputProps> = ({ count, error, onChange }) => {
       if (!currentValue) {
         inputsRef.current[currentInput].value = pastedCharacter;
         if (inputsRef.current[currentInput].nextElementSibling !== null) {
-          (inputsRef.current[currentInput].nextElementSibling as HTMLInputElement).focus();
+          (
+            inputsRef.current[currentInput]
+              .nextElementSibling as HTMLInputElement
+          ).focus();
           currentInput++;
         }
       }
@@ -68,7 +82,7 @@ const CodeInput: FC<CodeinputProps> = ({ count, error, onChange }) => {
 
   const calculateInputs = () => {
     return new Array(count)
-      .fill('')
+      .fill("")
       .map((_, i) => (
         <input
           type="text"
@@ -83,7 +97,7 @@ const CodeInput: FC<CodeinputProps> = ({ count, error, onChange }) => {
           onChange={handleChange}
           id={String(i)}
           max="9"
-          ref={(e: HTMLInputElement) => (inputsRef.current[i] = e)}
+          ref={(e: any) => (inputsRef.current[i] = e)}
           autoComplete="one-time-code"
         />
       ));
@@ -91,7 +105,9 @@ const CodeInput: FC<CodeinputProps> = ({ count, error, onChange }) => {
 
   return (
     <div className={styles.root}>
-      <span className={`${styles.inputsContainer} ${error ? styles.error : ''}`}>
+      <span
+        className={`${styles.inputsContainer} ${error ? styles.error : ""}`}
+      >
         {calculateInputs()}
       </span>
       {error && <span className={styles.error}>{error}</span>}
