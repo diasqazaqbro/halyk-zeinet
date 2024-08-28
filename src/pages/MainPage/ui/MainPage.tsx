9bcdd076785c09a66f8bfe61cee79da388c4c120
Bagdat <clonn262@gmail.com>
2024-08-28 16:18:14 +0500

    fix

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,23 +1,45 @@
-import Annuity from "@/widgets/Annuity/ui/Annuity";
-import { Benefits } from "@/widgets/Benefits";
-import Faq from "@/widgets/FAQ/ui/FAQ";
-import Oppv from "@/widgets/Oppv/ui/Oppv";
-import OpvOppv from "@/widgets/OpvOppv/ui/OpvOppv";
-import PensionAnnuity from "@/widgets/PensionAnnuity/ui/PensionAnnuity";
-import { Security } from "@/widgets/Security";
+import dynamic from "next/dynamic";
+
+const PensionAnnuity = dynamic(
+  () => import("@/widgets/PensionAnnuity/ui/PensionAnnuity"),
+  {
+    ssr: false,
+  }
+);
+
+const Annuity = dynamic(() => import("@/widgets/Annuity/ui/Annuity"), {
+  ssr: false,
+});
+
+const Oppv = dynamic(() => import("@/widgets/Oppv/ui/Oppv"), {
+  ssr: false,
+});
+
+const OpvOppv = dynamic(() => import("@/widgets/OpvOppv/ui/OpvOppv"), {
+  ssr: false,
+});
+
+const Benefits = dynamic(() => import("@/widgets/Benefits/ui/Benefits"), {
+  ssr: false,
+});
+
+const Security = dynamic(() => import("@/widgets/Security/ui/Security"), {
+  ssr: false,
+});
+
+const Faq = dynamic(() => import("@/widgets/FAQ/ui/FAQ"), {
+  ssr: false,
+});
 
 export default function MainPage() {
   return (
     <>
       <PensionAnnuity />
       <Annuity />
-
       <Oppv />
       <OpvOppv />
-
       <Benefits />
       <Security />
-
       <Faq />
     </>
   );
