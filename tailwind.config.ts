6884b994234d88e7e9620c183dc82c81dd8cb88b
Bagdat <clonn262@gmail.com>
2024-08-28 21:03:06 +0500

    fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,7 @@ const { nextui } = require("@nextui-org/react");
 const config: Config = {
   content: [
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@nextui-org/theme/dist/components/modal.{js,ts,jsx,tsx}",
   ],
   theme: {
     screens: {
