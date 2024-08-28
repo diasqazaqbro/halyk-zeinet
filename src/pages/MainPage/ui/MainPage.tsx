c57a383067e1beab263887e7100a17ab0f9b8bf5
Bagdat <clonn262@gmail.com>
2024-08-28 18:29:27 +0500

    fix

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -6,8 +6,12 @@ const PensionAnnuity = React.memo(
 const Annuity = React.memo(lazy(() => import("@/widgets/Annuity/ui/Annuity")));
 const Oppv = React.memo(lazy(() => import("@/widgets/Oppv/ui/Oppv")));
 const OpvOppv = React.memo(lazy(() => import("@/widgets/OpvOppv/ui/OpvOppv")));
-const Benefits = lazy(() => import("@/widgets/Benefits/ui/Benefits"));
-const Security = lazy(() => import("@/widgets/Security/ui/Security"));
+const Benefits = React.memo(
+  lazy(() => import("@/widgets/Benefits/ui/Benefits"))
+);
+const Security = React.memo(
+  lazy(() => import("@/widgets/Security/ui/Security"))
+);
 const Faq = lazy(() => import("@/widgets/FAQ/ui/FAQ"));
 
 export default function MainPage() {
