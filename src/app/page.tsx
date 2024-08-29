5e1efa7133be27c9a7e973d4299405040109ccba
Bagdat <clonn262@gmail.com>
2024-08-29 13:01:06 +0500

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
+import dynamic from "next/dynamic";
 import MainPage from "@/pages/MainPage";
-import Footer from "@/widgets/Footer/ui/Footer";
+
+const Footer = dynamic(() => import("@/widgets/Footer/ui/Footer"), {
+  ssr: false,
+  loading: () => <div className="text-center">Loading Footer...</div>,
+});
 
 export default function Home() {
   return (
